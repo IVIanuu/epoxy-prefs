91146f163ed7f826b0932666a0cd2b413b564fa6
IVIanuu <IVIanuu@gmail.com>
2018-09-21 02:34:28 +0200

    Updated kotlin

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -9,15 +9,10 @@ object Versions {
     const val versionName = "1.0"
 
     const val androidGradlePlugin = "3.2.0-rc03"
-
     const val androidx = "1.0.0-rc02"
-
     const val epoxy = "2.16.4"
-
-    const val kotlin = "1.2.61"
-
+    const val kotlin = "1.3.0-rc-57"
     const val materialDialogs = "0.9.6.0"
-
     const val mavenGradle = "2.1"
 }
 
@@ -29,7 +24,7 @@ object Deps {
     const val epoxy = "com.airbnb.android:epoxy:${Versions.epoxy}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
     const val materialDialogs = "com.afollestad.material-dialogs:core:${Versions.materialDialogs}"
 
