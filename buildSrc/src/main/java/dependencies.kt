df1f539a0dd49cdea470ac0b7718f92b460e39c4
IVIanuu <IVIanuu@gmail.com>
2018-09-27 23:28:12 +0200

    Local maven

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -6,11 +6,12 @@ object Versions {
     const val minSdk = 17
     const val targetSdk = 28
     const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionName = "SNAPSHOT"
+    const val groupId = "com.ivianuu.epoxyprefs"
 
-    const val androidGradlePlugin = "3.2.0-rc03"
-    const val androidx = "1.0.0-rc02"
-    const val epoxy = "2.16.4"
+    const val androidGradlePlugin = "3.2.0"
+    const val androidx = "1.0.0"
+    const val epoxy = "2.18.0"
     const val kotlin = "1.3.0-rc-57"
     const val materialDialogs = "0.9.6.0"
     const val mavenGradle = "2.1"
